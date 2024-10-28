c7942cc73ea77e418de5e3082924e58fc3f522c7
urassh <shuto.1011@icloud.com>
2024-10-28 16:57:54 +0900

    refactor: hero.vueに分割

---
+++ pages/hero.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="hero">
+        <h1>urassh</h1>
+        <h1>Code Portfolio</h1>
+        <p>iOS / Flutter</p>
+        <p>Web Development</p>
+        <p>Unity</p>
+    </div>
+</template>
+
+<style scoped>
+#hero {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: start;
+    padding: 0 64px;
+    h1 {
+        font-size: 84px;
+        font-weight: bold;
+        letter-spacing: 0.1em;
+        margin-bottom: 16px;
+    }
+
+    p {
+        font-size: 24px;
+        font-weight: 500;
+        margin-top: 16px;
+        opacity: 0.6;
+        font-weight: lighter;
+        letter-spacing: 0.08em;
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Header from './header.vue';
+import Hero from './hero.vue';
 
 const SITE_TITLE = "urassh@developer";
 
@@ -20,13 +21,7 @@ useHead(
     <Header />
 
     <main id="back-pattern" class="boxes">
-        <div id="hero">
-            <h1>urassh</h1>
-            <h1>Code Portfolio</h1>
-            <p>iOS / Flutter</p>
-            <p>Web Development</p>
-            <p>Unity</p>
-        </div>
+        <Hero />
     </main>
 </template>
 
@@ -63,47 +58,6 @@ useHead(
     }
     to {
         background-position: 100% 0%;
-    }
-}
-
-header {
-    background-color: rgba(0, 0, 0, 0.5);
-    padding: 12px 32px;
-    position: fixed;
-    top: 0;
-    width: 100%;
-    .header-title {
-        display: inline-block;
-        font-weight: 600;
-        font-size: 42px;
-        background: linear-gradient(90deg, #8A17FC 24%, #5CFFFF 100%);
-        background-clip: text;
-        -webkit-text-fill-color: transparent;
-    }
-}
-
-#hero {
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: start;
-    padding: 0 64px;
-    h1 {
-        font-size: 84px;
-        font-weight: bold;
-        letter-spacing: 0.1em;
-        margin-bottom: 16px;
-    }
-
-    p {
-        font-size: 24px;
-        font-weight: 500;
-        margin-top: 16px;
-        opacity: 0.6;
-        font-weight: lighter;
-        letter-spacing: 0.08em;
     }
 }
 </style>
