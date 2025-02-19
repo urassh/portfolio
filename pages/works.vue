d1061990fd605db5c0d94f6cca0313b5f09183a8
urassh <shuto.1011@icloud.com>
2025-02-19 17:15:30 +0900

    fix: background

--- pages/works.vue
+++ pages/works.vue
@@ -120,7 +120,7 @@ const closeModal = () => {
     left: 0;
     width: 100%;
     height: 100%;
-    background: rgba(0, 0, 0, 0.5);
+    background: rgba(0, 0, 0, 0.7);
     display: flex;
     justify-content: center;
     align-items: center;
